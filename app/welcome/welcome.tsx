cfdd868a56ae097034768d32211639fdfc66a3ff
Varinder Sharma <92428994+abhaygoswami302@users.noreply.github.com>
2025-01-15 13:41:00 +0530

    2nd changes

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,16 @@
-
-
 export function Welcome() {
   return (
-  <h1>Welcome ghjjk Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt beatae veritatis recusandae.</h1>
+    <>
+      <h1>
+        Welcome ghjjk Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Deserunt beatae veritatis recusandae.
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
+        perspiciatis qui velit error impedit. At, voluptatum perferendis, quos
+        aliquam itaque vel odio consequatur esse aliquid vero, iusto pariatur
+        autem rem.
+      </p>
+    </>
   );
 }
