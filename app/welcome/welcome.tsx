ef15aae60ac7f637af8f96713486c8311d4dd3ff
Varinder Sharma <92428994+abhaygoswami302@users.noreply.github.com>
2025-01-17 10:59:28 +0530

    auth-code

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,23 +1,59 @@
-export function Welcome() {
+import {
+  useLoaderData,
+  Form,
+  type LoaderFunctionArgs,
+  type ActionFunctionArgs,
+} from "react-router";
+import axios from "axios";
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
+interface InputDataProps {
+  name: string | null;
+  email: string | null;
+  age: number | "";
+}
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    if (!res.ok) {
+      throw new Error("failed to fetch data");
+    }
+    const data = res.json();
+    console.log("response data:" + data);
+    return data;
+  } catch (error) {
+    console.log("Error in loader:" + error);
+    return null;
+  }
+};
+
+export default function Welcome() {
+  const data = useLoaderData();
+
   return (
     <>
-      <h1>
-        Welcome ghjjk Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Deserunt beatae veritatis recusandae.
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
-        perspiciatis qui velit error impedit. At, voluptatum perferendis, quos
-        aliquam itaque vel odio consequatur esse aliquid vero, iusto pariatur
-        autem rem.
-
-
-      
-      </p>
-      <div>
-        <h1>grid my branchofbranch </h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta nobis consequuntur molestias aspernatur suscipit! Qui blanditiis, expedita temporibus doloremque nulla incidunt hic doloribus, vero repellendus dolor aliquam ad atque non.</p>
-      </div>
+      <Form method="post">
+        <div className="flex flex-col justify-start"></div>
+        <div className="mb-2">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="border" id="name" name="name" />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="border" id="email" name="email" />
+        </div>
+        <div className="mb-2">
+          <label htmlFor="Age">Age</label>
+          <input type="number" className="border" id="Age" name="age" />
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
     </>
   );
 }
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const body = await request.json();
+  console.log(body);
+};
